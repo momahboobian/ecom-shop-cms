53e65be15c50e27588e3018e840b6c97c8deec00
Mo Mahboobian <momahboobian@gmail.com>
2024-01-07 17:43:39 +0000

    Implementing the header nav bar

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../MobileNav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            className="{classes.log}"
+            width={170}
+            height={50}
+          />
+        </Link>
+
+        <HeaderNav header={header} />
+
+        {/* <MobileNav header={header} /> */}
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
