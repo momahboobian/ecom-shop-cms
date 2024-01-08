6b368d2bd7fd5f6377592877462112cc249f6ca8
Mo Mahboobian <momahboobian@gmail.com>
2024-01-08 15:56:08 +0000

    Updating the production cagories

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 
 import { Category } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
@@ -11,11 +14,14 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
+
+  const { setCategoryFilters } = useFilter()
   return (
     <Link
       href="/products"
       className={classes.card}
       style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
