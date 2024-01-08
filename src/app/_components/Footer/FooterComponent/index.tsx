3dd436e10769017481d3e5a5506791d9e6a92680
Mo Mahboobian <momahboobian@gmail.com>
2024-01-08 12:12:27 +0000

    Updating footer icons and link

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -5,14 +5,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import { Footer } from '../../../../payload/payload-types'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { Footer, Media } from '../../../../payload/payload-types'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
 import { Gutter } from '../../Gutter'
 
 import classes from './index.module.scss'
+import { Button } from '../../Button'
 
 const FooterComponent = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
+
+  const navItems = footer?.navItems || []
 
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
@@ -41,6 +44,29 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
               <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
             </Link>
             <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                return (
+                  <Button
+                    key={item.link.label}
+                    el={'link'}
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
           </div>
         </Gutter>
       </div>
