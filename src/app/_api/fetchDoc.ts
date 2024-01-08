3842b0949e9729d939d1d13fa620ac50bfb2df3e
Mo Mahboobian <momahboobian@gmail.com>
2024-01-08 14:07:23 +0000

    Uodating graph schema categories

--- src/app/_api/fetchDoc.ts
+++ src/app/_api/fetchDoc.ts
@@ -4,6 +4,7 @@ import type { Config } from '../../payload/payload-types'
 import { ORDER } from '../_graphql/orders'
 import { PAGE } from '../_graphql/pages'
 import { PRODUCT } from '../_graphql/products'
+import { CATEGORIES } from '../_graphql/categories'
 import { GRAPHQL_API_URL } from './shared'
 import { payloadToken } from './token'
 
@@ -19,6 +20,10 @@ const queryMap = {
   orders: {
     query: ORDER,
     key: 'Orders',
+  },
+  categories: {
+    query: CATEGORIES,
+    key: 'categories',
   },
 }
 
