e415161c98fb51b1d16d74cd3b86188b6382d3af
Mo Mahboobian <momahboobian@gmail.com>
2024-01-09 12:53:54 +0000

    Implementing custom RadioButton

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+'use client'
 
-import classes from './index.modul.scss'
+import React, { useState } from 'react'
 
-const Filters = () => {
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
+import { HR } from '../../../_components/HR'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([])
+
+  const handleCategories = (categoryId: string) => {
+    const isSelected = selectedCategories.includes(categoryId)
+    const updatedSelection = isSelected
+      ? selectedCategories.filter(id => id !== categoryId)
+      : [...selectedCategories, categoryId]
+
+    setSelectedCategories(updatedSelection)
+  }
+
   return (
     <div className={classes.filters}>
-      <div>something</div>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={() => handleCategories(category.id)}
+              />
+            )
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}></h6>
+        <div className={classes.categories}>
+          <RadioButton />
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -5,6 +5,7 @@ import qs from 'qs'
 
 import type { Product } from '../../../payload/payload-types'
 import type { ArchiveBlockProps } from '../../_blocks/ArchiveBlock/types'
+import { useFilter } from '../../_providers/Filter'
 import { Card } from '../Card'
 import { Gutter } from '../Gutter'
 import { PageRange } from '../PageRange'
@@ -38,6 +39,8 @@ export type Props = {
 }
 
 export const CollectionArchive: React.FC<Props> = props => {
+  const { categoryFilters, sort } = useFilter()
+
   const {
     categories: catsFromProps,
     className,
@@ -49,7 +52,6 @@ export const CollectionArchive: React.FC<Props> = props => {
     relationTo,
     selectedDocs,
     showPageRange,
-    sort = '-createdAt',
   } = props
 
   const [results, setResults] = useState<Result>({
@@ -116,10 +118,10 @@ export const CollectionArchive: React.FC<Props> = props => {
           page,
           sort,
           where: {
-            ...(categories
+            ...(categoryFilters
               ? {
                   categories: {
-                    in: categories,
+                    in: categoryFilters,
                   },
                 }
               : {}),
@@ -162,7 +164,7 @@ export const CollectionArchive: React.FC<Props> = props => {
     return () => {
       if (timer) clearTimeout(timer)
     }
-  }, [page, categories, relationTo, onResultChange, sort, limit, populateBy])
+  }, [page, categoryFilters, relationTo, onResultChange, sort, limit, populateBy])
 
   return (
     <div className={[classes.collectionArchive, className].filter(Boolean).join(' ')}>
